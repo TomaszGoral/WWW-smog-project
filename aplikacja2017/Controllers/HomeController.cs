5a78f7e7d8008a8c7bdf093ed4bfedd01b98057e
Tomasz Goral <goralt@student.mini.pw.edu.pl>
2019-02-13 16:38:14 +0100

    v 1.0

--- aplikacja2017/Controllers/HomeController.cs
+++ aplikacja2017/Controllers/HomeController.cs
@@ -14,137 +14,263 @@ namespace aplikacja2017.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            User responseuser = new User { user = "", password = "" };
+            return View((object)responseuser);
         }
         public ActionResult List()
         {
             return View();
         }
-        // GET: Home/AddPoint
-        public ActionResult AddPoint(Point point)
+        public ActionResult Account()
         {
-            string name = "unknown";
-            Int32 saveResult;
-            List<string> addresslist = new List<string>();
-
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Addaccount(string auser, string apassword, string arepassword)
+        {
+            User responseuser = new User { user = "", password = "" };
+            if (apassword != arepassword) return Content("not identical passwords");
+            bool usertaken = false;
             using (NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;" +
                             "User Id=postgres;Password=goralt;Database=postgis;"))
             {
-
-                string punkt = "ST_Transform(ST_GeomFromText('POINT(" +
-                    point.Lon.ToString(CultureInfo.InvariantCulture) + " " + point.Lat.ToString(CultureInfo.InvariantCulture) + ")',3857),2180)";
-
                 conn.Open();
-
-                string areaQuery = "(SELECT geom FROM warszawa WHERE ST_Contains(warszawa.geom," + punkt + " ))";
-                string addressQuery = "SELECT adres FROM adresy WHERE ST_Contains(" + areaQuery + ",ST_Transform(adresy.geom,2180))";
-                var command = new NpgsqlCommand(addressQuery, conn);
-                int i = 0;
-                
+                string loginQuery = "(SELECT \"user\", pass, latestactivity FROM users)";
+                var command = new NpgsqlCommand(loginQuery, conn);
                 using (NpgsqlDataReader dr = command.ExecuteReader())
                 {
                     while (dr.Read())
                     {
-                        addresslist.Add(dr.GetValue(0).ToString());
+
+                        if (dr[0].ToString() == auser)
+                        {
+                            usertaken = true;
+                        }
                     }
                 }
+                if (usertaken) return Content("Username taken");
+                string insertQuery = "INSERT INTO users(\"user\", pass, latestactivity) VALUES('" + auser + "', '" + apassword + "', '1970-01-01 08:00:00')";
+                var insertcommand = new NpgsqlCommand(insertQuery, conn);
+                Int32 save = insertcommand.ExecuteNonQuery();
+                responseuser = new User { user = auser, password = apassword };
+                return View("Index", (object)responseuser);
 
-                if (name != "undefined")
-                {
-                    
-                    //int w = 1;
-                    //string Query = "(SELECT address FROM points WHERE address='" + name + "')";
-                    //var com = new NpgsqlCommand(Query, conn);
-                    //string firstrecord="";
-                    //using (NpgsqlDataReader dr = com.ExecuteReader())
-                    //{
-                    //    if(dr.Read())
-                    //    {
-                    //        while(dr.GetValue(i++)!=null)
-                    //        string s;
-                    //        addresslist.Add(dr.GetValue(0).ToString());
-                    //    }
-                    //}
-                    //if (firstrecord == "")
-                    //{
-                    //    string insertQuery = "insert into points(address,geom,p) values (\'" + name + "\'," + punkt + "," + w.ToString() + ")";
-                    //    var insertComm = new NpgsqlCommand(insertQuery, conn);
-                    //    saveResult = insertComm.ExecuteNonQuery();
-                    //}
-                    //else
-                    //{
-                    //    string insertQuery = "update points set p=p+" + w.ToString() + " where address='" + name + "'";
-                    //    var insertComm = new NpgsqlCommand(insertQuery, conn);
-                    //    saveResult = insertComm.ExecuteNonQuery();
-                    //}
-                }
             }
-
-            var commune = new
-            {
-                Name = name,
-                //Saved = saveResult
-            };
-
-            return Json(addresslist);
+            
+            return RedirectToAction("Index",responseuser);
         }
-        public ActionResult AddAddress(_address name)
+        [HttpPost]
+        public ActionResult Index(string suser, string spassword)
         {
-            _address response = new _address { Address = "", Lat = 0, Lon = 0 ,Points=0 };
+            User responseuser = new User { user = "", password = "" };
             using (NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;" +
                             "User Id=postgres;Password=goralt;Database=postgis;"))
             {
-                
-                if (name.Address != "")
+                conn.Open();
+                string loginQuery = "(SELECT \"user\", pass, latestactivity FROM users)";
+                var command = new NpgsqlCommand(loginQuery, conn);
+                bool isinbase = false;
+
+                using (NpgsqlDataReader dr = command.ExecuteReader())
                 {
-                    conn.Open();
-                    double w = name.Points;
-                    string point = "ST_Transform(ST_GeomFromText('POINT(" +
-                    name.Lon.ToString(CultureInfo.InvariantCulture) + " " + name.Lat.ToString(CultureInfo.InvariantCulture) + ")',3857),2180)";
-                    string areaQuery = "(SELECT geom FROM warszawa WHERE ST_Contains(warszawa.geom," + point + " ))";
-                    string addressQuery = "SELECT adres FROM adresy WHERE ST_Contains(" + areaQuery + ",ST_Transform(adresy.geom,2180))";
-                    string Query = "(SELECT address FROM points WHERE address='" + name.Address + "' and ST_Contains(" + areaQuery + ",points.geom))";
-                    string Querygeom = "(SELECT geom FROM adresy WHERE adres='" + name.Address + "' and ST_Contains(" + areaQuery + ",ST_Transform(adresy.geom,2180)))";
-                    var com = new NpgsqlCommand(Query, conn);
-                    string firstrecord = "";
-                    NpgsqlTypes.PostgisPoint geometry =null;
-                    Int32 saveResult;
-                    using (NpgsqlDataReader dr = com.ExecuteReader())
+                    while (dr.Read())
                     {
-                        if (dr.Read())
+                        
+                        if (dr[0].ToString() == suser && dr[1].ToString() == spassword )
                         {
-                            firstrecord=dr.GetValue(0).ToString();
-                            
+                            isinbase = true;
+                            responseuser = new User { user = suser, password = spassword };
                         }
-                    }
-                    com = new NpgsqlCommand(Querygeom, conn);
-                    using (NpgsqlDataReader dr = com.ExecuteReader())
-                    {
-                        if (dr.Read())
-                        {
-                            geometry= (NpgsqlTypes.PostgisPoint)dr.GetValue(0);
-                        }
-                    }
-                    if (firstrecord == "")
-                    {
-                        string punkt = "ST_Transform(ST_GeomFromText('POINT(" +
-                           geometry.X.ToString(CultureInfo.InvariantCulture) + " " + geometry.Y.ToString(CultureInfo.InvariantCulture) + ")',2178),2180)";
-                        string insertQuery = "insert into points(address,geom,p) values ('" + name.Address + "'," + punkt + "," + w.ToString() + ")";
-                        var insertComm = new NpgsqlCommand(insertQuery, conn);
-                        response.Address = name.Address;
-                        response.Lat = geometry.X;
-                        response.Lon = geometry.Y;
-                        saveResult = insertComm.ExecuteNonQuery();
-                    }
-                    else
-                    {
-                        string insertQuery = "update points set p=p+" + w.ToString() + " where address='" + name.Address + "'";
-                        var insertComm = new NpgsqlCommand(insertQuery, conn);
-                        saveResult = insertComm.ExecuteNonQuery();
                     }
                 }
+                
+            }
+            return View((object)responseuser);
+        }
+        // GET: Home/AddPoint
+        /*public ActionResult AddPoint(PointPlusUser point)
+        {
+            List<string> addresslist = new List<string>();
+            bool logged = false;
+            using (NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;" +
+                            "User Id=postgres;Password=goralt;Database=postgis;"))
+            {
+                conn.Open();
+                string loginQuery = "(SELECT \"user\", pass, latestactivity FROM users)";
+                var command = new NpgsqlCommand(loginQuery, conn);
+                using (NpgsqlDataReader dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        
+                        if (dr[0].ToString() == point.user && dr[1].ToString() == point.password && DateTime.Compare(((DateTime)dr[2]).Date, DateTime.Now.Date) < 0)
+                        {
+                            logged = true;
+                        }
+                    }
+                }
+
+            }
+            if (logged)
+            {
+                string name = "unknown";
+                Int32 saveResult;
+                
+
+                using (NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;" +
+                                "User Id=postgres;Password=goralt;Database=postgis;"))
+                {
+
+                    string punkt = "ST_Transform(ST_GeomFromText('POINT(" +
+                        point.Lon.ToString(CultureInfo.InvariantCulture) + " " + point.Lat.ToString(CultureInfo.InvariantCulture) + ")',3857),2180)";
+
+                    conn.Open();
+
+                    string areaQuery = "(SELECT geom FROM warszawa WHERE ST_Contains(warszawa.geom," + punkt + " ))";
+                    string addressQuery = "SELECT adres FROM adresy WHERE ST_Contains(" + areaQuery + ",ST_Transform(adresy.geom,2180))";
+                    var command = new NpgsqlCommand(addressQuery, conn);
+                    int i = 0;
+
+                    using (NpgsqlDataReader dr = command.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            addresslist.Add(dr.GetValue(0).ToString());
+                        }
+                    }
+
+                    if (name != "undefined")
+                    {
+
+                        //int w = 1;
+                        //string Query = "(SELECT address FROM points WHERE address='" + name + "')";
+                        //var com = new NpgsqlCommand(Query, conn);
+                        //string firstrecord="";
+                        //using (NpgsqlDataReader dr = com.ExecuteReader())
+                        //{
+                        //    if(dr.Read())
+                        //    {
+                        //        while(dr.GetValue(i++)!=null)
+                        //        string s;
+                        //        addresslist.Add(dr.GetValue(0).ToString());
+                        //    }
+                        //}
+                        //if (firstrecord == "")
+                        //{
+                        //    string insertQuery = "insert into points(address,geom,p) values (\'" + name + "\'," + punkt + "," + w.ToString() + ")";
+                        //    var insertComm = new NpgsqlCommand(insertQuery, conn);
+                        //    saveResult = insertComm.ExecuteNonQuery();
+                        //}
+                        //else
+                        //{
+                        //    string insertQuery = "update points set p=p+" + w.ToString() + " where address='" + name + "'";
+                        //    var insertComm = new NpgsqlCommand(insertQuery, conn);
+                        //    saveResult = insertComm.ExecuteNonQuery();
+                        //}
+                    }
+                    
+
+                        string loginUpdateQuery = "UPDATE users SET latestactivity = NOW() WHERE \"user\" ='" + point.user + "' AND pass='" + point.password + "'";
+                        var upcommand = new NpgsqlCommand(loginUpdateQuery, conn);
+                        Int32 save = upcommand.ExecuteNonQuery();
+                    
+                }
+
+                var commune = new
+                {
+                    Name = name,
+                    //Saved = saveResult
+                };
+            }
+            return Json(addresslist);
+        }*/
+        public ActionResult AddAddress(_address name)
+        {
+            bool logged = false;
+            using (NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;" +
+                            "User Id=postgres;Password=goralt;Database=postgis;"))
+            {
+                conn.Open();
+                string loginQuery = "(SELECT \"user\", pass, latestactivity FROM users)";
+                var command = new NpgsqlCommand(loginQuery, conn);
+                using (NpgsqlDataReader dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+
+                        if (dr[0].ToString() == name.User && dr[1].ToString() == name.Password && DateTime.Compare(((DateTime)dr[2]).Date, DateTime.Now.Date) < 0)
+                        {
+                            logged = true;
+                        }
+                    }
+                }
+
             }
 
+            _address response = new _address { Address = "", Lat = 0, Lon = 0 ,Points=0, User="", Password="" };
+            if (logged)
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;" +
+                                "User Id=postgres;Password=goralt;Database=postgis;"))
+                {
+
+                    if (name.Address != "")
+                    {
+                        conn.Open();
+                        double w = name.Points;
+                        
+                        /*string areaQuery = "(SELECT geom FROM warszawa WHERE ST_Contains(warszawa.geom," + point + " ))";
+                        string addressQuery = "SELECT adres FROM adresy WHERE ST_Contains(" + areaQuery + ",ST_Transform(adresy.geom,2180))";
+                        string Query = "(SELECT address FROM points WHERE address='" + name.Address + "' and ST_Contains(" + areaQuery + ",points.geom))";
+                        string Querygeom = "(SELECT geom FROM adresy WHERE adres='" + name.Address + "' and ST_Contains(" + areaQuery + ",ST_Transform(adresy.geom,2180)))";*/
+                        string Query = "SELECT address from points where address ='" + name.Address +"'";
+                        var com = new NpgsqlCommand(Query, conn);
+                        string firstrecord = "";
+                        NpgsqlTypes.PostgisPoint geometry = null;
+                        Int32 saveResult;
+                        using (NpgsqlDataReader dr = com.ExecuteReader())
+                        {
+                            if (dr.Read())
+                            {
+                                firstrecord = dr.GetValue(0).ToString();
+
+                            }
+                        }
+                        /*com = new NpgsqlCommand(Querygeom, conn);
+                        using (NpgsqlDataReader dr = com.ExecuteReader())
+                        {
+                            if (dr.Read())
+                            {
+                                geometry = (NpgsqlTypes.PostgisPoint)dr.GetValue(0);
+                            }
+                        }*/
+                        if (firstrecord == "")
+                        {
+                            /*string punkt = "ST_Transform(ST_GeomFromText('POINT(" +
+                               geometry.X.ToString(CultureInfo.InvariantCulture) + " " + geometry.Y.ToString(CultureInfo.InvariantCulture) + ")',2178),2180)";*/
+                            string point = "ST_Transform(ST_GeomFromText('POINT(" +
+                            name.Lon.ToString(CultureInfo.InvariantCulture) + " " + name.Lat.ToString(CultureInfo.InvariantCulture) + ")',3857),2180)";
+                            string insertQuery = "insert into points(address,geom,p,count) values ('" + name.Address + "'," + point + "," + w.ToString() + ",1)";
+                            var insertComm = new NpgsqlCommand(insertQuery, conn);
+                            response.Address = name.Address;
+                            response.Lat = name.Lat;
+                            response.Lon = name.Lon;
+                            saveResult = insertComm.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            string insertQuery = "update points set p=p+" + w.ToString() + ", count=count+1 where address='" + name.Address + "'";
+                            var insertComm = new NpgsqlCommand(insertQuery, conn);
+                            saveResult = insertComm.ExecuteNonQuery();
+                            response.Address = "Update";
+                        }
+                        string loginUpdateQuery = "UPDATE users SET latestactivity = NOW() WHERE \"user\" ='" + name.User + "' AND pass='" + name.Password + "'";
+                        var upcommand = new NpgsqlCommand(loginUpdateQuery, conn);
+                        Int32 save = upcommand.ExecuteNonQuery();
+                    }
+                }
+                
+            }
             return Json(response); 
         }
         public ActionResult GetAllPoints()
@@ -203,12 +329,12 @@ namespace aplikacja2017.Controllers
             {
                 int i = 0;
                 conn.Open();
-                string query = "select address,p from points order by p desc";
+                string query = "select address,p,count from points order by p desc";
                 var command = new NpgsqlCommand(query, conn);
                 NpgsqlDataReader dr = command.ExecuteReader();
                 while (dr.Read() && i< 20)
                 {
-                    _ad p = new _ad() { Address = dr.GetValue(0).ToString(), Lat = dr.GetValue(1).ToString(), Lon = "" };
+                    _ad p = new _ad() { Address = dr.GetValue(0).ToString(), Lat = dr.GetValue(1).ToString(), Lon = dr.GetValue(2).ToString() };
                     lista.Add(p);
 
                 }
--- aplikacja2017/Models/Point.cs
+++ aplikacja2017/Models/Point.cs
@@ -5,11 +5,12 @@ using System.Web;
 
 namespace aplikacja2017.Models
 {
-    public class Point
+    public class PointPlusUser
     {
         public double Lat { get; set; }
         public double Lon { get; set; }
-        
+        public string user { get; set; }
+        public string password { get; set; }
     }
     public class _address
     {
@@ -17,6 +18,8 @@ namespace aplikacja2017.Models
         public double Lat { get; set; }
         public double Lon { get; set; }
         public double Points { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
     }
     public class _ad
     {
